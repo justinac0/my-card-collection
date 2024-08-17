bc5c120761da423d5a38089b1cec8b8b3995e565
justinac0 <justinac.1999@gmail.com>
2024-08-17 21:52:16 +1000

    Updated Schema.

--- src/app/lib/db/schema.ts
+++ src/app/lib/db/schema.ts
@@ -1,14 +1,29 @@
-import { mysqlTable, varchar, serial } from "drizzle-orm/mysql-core";
+import { mysqlTable, varchar, serial, int, text } from "drizzle-orm/mysql-core";
+import { relations } from "drizzle-orm";
 
 export const users = mysqlTable('users', {
     id: serial('id').primaryKey(),
     firstname: varchar('firstname', {length: 64}).notNull(),
     lastname: varchar('lastname', {length: 64}).notNull(),
     email: varchar('email', {length: 64}).notNull().unique(),
-    // passwordHash: varchar('passwordHash', {length: 256}).notNull(),
+    passwordHash: varchar('passwordHash', {length: 256}).notNull(),
 });
+
+export const usersRelations = relations(users, ({ many }) => ({
+    cards: many(cards),
+}));
 
 export const cards = mysqlTable('cards', {
     id: serial('id').primaryKey(),
     name: varchar('name', {length: 64}).notNull(),
+    description: text('description'),
+    count: int('count'),
+    owner_id: int('owner_id')
 });
+
+export const cardsRelations = relations(cards, ({ one }) => ({
+    id: one(users, {
+        fields: [cards.owner_id],
+        references: [users.id]
+    }),
+}));
